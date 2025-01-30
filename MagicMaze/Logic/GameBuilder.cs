1d4cf33f39eeec5ab41612058a9256a1e161f4ac
cartaya.liz <cartayaliz2006@gmail.com>
2025-01-30 15:29:03 -0500

    Organizando

---
+++ MagicMaze/Logic/GameBuilder.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+
+namespace Logic
+{
+    public class GameBuilder
+    {
+
+        public static GameCenter TestGame(IVisual visual)
+        {
+
+
+            Player player1 = new PlayerBuho();
+            Player player2 = new PlayerCorrecamino();
+            Player player3 = new PlayerExplorador();
+
+
+            Board board = new Board(10);
+
+            board.setPlayer(0, 4, player1);
+
+            board.setPlayer(4, 3, player2);
+            board.setPlayer(2, 5, player3);
+
+            board.matrix[0, 0] = new CellObs(0, 0);
+            board.matrix[9, 9] = new CellWinn(9, 9);
+            board.matrix[3, 3] = new CellSpeed1(3, 3);
+            board.matrix[2, 2] = new CellSpeed2(2, 2);
+            board.matrix[1, 3] = new CellVision(1, 3);
+            board.matrix[1, 1] = new CellBridge(1, 1);
+
+
+            GameCenter gc = new GameCenter(board, new List<Player> { player1, player2, player3 }, visual);
+
+            return gc;
+
+        }
+    }
+
+}
--- MagicMaze/Logic/Program.cs
+++ MagicMaze/Logic/Program.cs
@@ -1,29 +1,9 @@
 ﻿using Logic;
 
 
-Player player1 = new PlayerBuho();
-Player player2 = new PlayerCorrecamino();
-Player player3 = new PlayerExplorador();
-
-
-Board board = new Board(20);
-
-board.setPlayer(0, 4, player1);
-
-board.setPlayer(4, 3, player2);
-board.setPlayer(12, 5, player3);
-
-board.matrix[0, 0] = new CellObs(0, 0);
-board.matrix[19, 19] = new CellWinn(19, 19);
-board.matrix[3, 3] = new CellSpeed1(3, 3);
-board.matrix[2, 2] = new CellSpeed2(2, 2);
-board.matrix[1, 3] = new CellVision(1, 3);
-board.matrix[1, 1] = new CellBridge(1, 1);
-
-
 BasicConsoleVisual game = new BasicConsoleVisual();
 
-GameCenter gc = new GameCenter(board, new List<Player> { player1, player2, player3 }, game);
+var gc = GameBuilder.TestGame(game);
 
 game.Play(gc);
 
